7af74bf46db84eab3483d8b91328ee0599a8ab66
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2024-01-05 19:28:57 +0300

    Add project files.

---
+++ HexAndBit/ConverterHexAndBinary.cs
@@ -0,0 +1,168 @@
+﻿using System.Text;
+
+namespace HexAndBit
+{
+    public static class ConverterHexAndBinary
+    {
+        public static string HexToBinary(string hex)
+        {
+            var list=new List<string>();
+
+            if (string.IsNullOrEmpty(hex))
+                return hex;
+
+            var trimHex=hex.Trim();
+            string binary = "";
+            foreach (char c in trimHex)
+            {
+                int value = Convert.ToInt32(c.ToString(),16);
+                binary += Convert.ToString(value,2).PadLeft(4, '0');
+                
+            }
+
+            list.Add(binary);
+            return binary;
+        }
+
+        public static string HexToBinary2(string hex)
+        {
+            if (string.IsNullOrEmpty(hex) || !IsValidHex(hex))
+            {
+                Console.WriteLine("Hata: Geçerli bir onaltılık sayı değil.");
+                return hex;
+            }
+
+            string binaryString = string.Join(string.Empty,
+                hex.Select(c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')));
+
+            return binaryString;
+        }
+
+        private static bool IsValidHex(string hex)
+        {
+            foreach (char c in hex)
+            {
+                if (!IsHexadecimalDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool IsHexadecimalDigit(char c)
+        {
+            return (c >= '0' && c <= '9') ||
+                   (c >= 'A' && c <= 'F') ||
+                   (c >= 'a' && c <= 'f');
+        }
+
+        public static long HexToDecimal(string hex)
+        {
+            long decimalValue = 0;
+
+            if (IsValidHex(hex))
+            {
+                decimalValue = HexToDecimall(hex);
+                return decimalValue;
+            }
+            else
+            {
+                Console.WriteLine("Hatalı giriş! Geçerli bir onaltılık sayı giriniz.");
+            }
+
+            return decimalValue;
+        }
+        static long HexToDecimall(string hex)
+        {
+            return Convert.ToInt64(hex, 16);
+        }
+        public static string BinaryStringToHexString(string binary)
+        {
+            if (string.IsNullOrEmpty(binary))
+                return binary;
+
+            StringBuilder result = new StringBuilder(binary.Length / 8 + 1);
+
+            int mod4Len = binary.Length % 8;
+            if (mod4Len != 0)
+            {
+                // pad to length multiple of 8
+                binary = binary.PadLeft(((binary.Length / 8) + 1) * 8, '0');
+            }
+
+            for (int i = 0; i < binary.Length; i += 8)
+            {
+                string eightBits = binary.Substring(i, 8);
+                result.AppendFormat("{0:X2}", Convert.ToByte(eightBits, 2));
+            }
+
+            return result.ToString();
+        }
+
+        public static IEnumerable<String> SplitInParts(this String s, Int32 partLength)
+        {
+            if (s == null)
+                throw new ArgumentNullException(nameof(s));
+            if (partLength <= 0)
+                throw new ArgumentException("Part length has to be positive.", nameof(partLength));
+
+            for (var i = 0; i < s.Length; i += partLength)
+                yield return s.Substring(i, Math.Min(partLength, s.Length - i));
+        }
+
+        public static List<string> BitleriGruplaraAyir(string bitDizisi)
+        {
+            var list=new List<string>();
+            int grupSayisi = bitDizisi.Length / 8;
+            string[] grupluBitler = new string[grupSayisi];
+
+            for (int i = 0; i < grupSayisi; i++)
+            {
+                grupluBitler[i] = bitDizisi.Substring(i * 8, 8);
+                list.Add(bitDizisi.Substring(i * 8, 8));
+            }
+
+            return list;
+        }
+
+        public static List<TLVElement> ParseTLV(string hexData)
+        {
+            List<TLVElement> tlvElements = new List<TLVElement>();
+            int index = 0;
+
+            while (index < hexData.Length)
+            {
+                string tag = hexData.Substring(index, 2);
+                index += 2;
+
+                int length = Convert.ToInt32(hexData.Substring(index, 2), 16);
+                index += 2;
+
+                string value = hexData.Substring(index, length * 2);
+                index += length * 2;
+
+                TLVElement tlvElement = new TLVElement(tag, length, value);
+                tlvElements.Add(tlvElement);
+            }
+
+            return tlvElements;
+        }
+    }
+
+    public class TLVElement
+    {
+        public string Tag { get; }
+        public int Length { get; }
+        public string Value { get; }
+
+        public TLVElement(string tag, int length, string value)
+        {
+            Tag = tag;
+            Length = length;
+            Value = value;
+        }
+
+    }
+
+}
---
+++ HexAndBit/Program.cs
@@ -0,0 +1,147 @@
+﻿using HexAndBit;
+
+
+Console.WriteLine(" Hex to Binary Please press 'h'");
+
+string input = Console.ReadLine()!;
+
+while (input == "h")
+{
+    switch (input)
+    {
+        case "b":
+            Console.Write("Enter BinaryValue:");
+            var readBinaryValue = Console.ReadLine()!;
+            var value1 = ConverterHexAndBinary.BinaryStringToHexString(readBinaryValue);
+            Console.WriteLine($"HexValue:{value1}");
+            break;
+
+        case "h":
+            Console.Write("Enter HexadecimalValue:");
+            var readHexaDecimalValue = Console.ReadLine()!;
+            var h=readHexaDecimalValue.Trim();
+
+            var tagListValue = new List<TagList>();
+
+            int x = 0;
+            int y = 2;
+            int count = 0;
+
+            var tagValues=RecursiveLoopTagValues(x,y,h);
+
+            string RecursiveLoopTagValues(int x, int y,string h)
+            {
+                string byteData = "";
+                string subHex = "";
+                string tagValue = "";
+                string tagLengthValue = "";
+                string value = "";
+                string valueTag = "";
+                long decimalTagValue;
+                string gg = "";
+
+                ;
+
+
+                // YAPILACAK
+                if (h.Length == 0)
+                    return Dondur();
+                subHex = h.Substring(x, y);
+
+
+
+                if (subHex.Length>0)
+                     value = ConverterHexAndBinary.HexToBinary2(subHex);
+
+
+                    byteData = value.Substring(2);
+                    // Geri kalan işlemler
+
+
+                if ((byteData == "011111"))
+                {
+                   RecursiveLoopTagValues(x + 2, y, h);
+
+                }
+                else
+                {
+                    x = x + 2;
+
+                    tagValue = h?.Substring(0, x);
+                    // Geri kalan işlemler
+
+                    tagLengthValue = h?.Substring(x, 2);
+                   
+
+                    decimalTagValue = ConverterHexAndBinary.HexToDecimal(tagLengthValue);
+                    
+                    
+                    // Sıkıntı çıkartan yer
+                    valueTag = (h?.Substring(x + 2, (int)decimalTagValue * 2))!;
+
+                    tagListValue.Add(new TagList
+                    {
+                        TagValue = tagValue,
+                        TagLentghValue = tagLengthValue,
+                        DecimalTagValue = valueTag,
+                    });
+
+                    
+
+                    gg = h?.Substring(tagValue.Length+tagLengthValue.Length+(int)decimalTagValue*2);
+                    
+
+                    var foundItem = tagListValue.FirstOrDefault(x => x.TagValue == "70");
+                    
+                    // 9F'Lİ datanın buraya girmesi lazım.
+
+                    if(foundItem!=null && count == 0)
+                    {
+                        if (gg.Length > 0 && count != 0)
+                        {
+                            RecursiveLoopTagValues(x = 0, y, gg);
+                            count++;
+                        }
+
+                        else if (foundItem != null && count == 0)
+                            count++;
+                        RecursiveLoopTagValues(x = 0, y, valueTag);
+                        
+                    }
+                    else
+                    {
+                        RecursiveLoopTagValues(x = 0, y, gg);
+                    }
+
+                }
+                return "";
+
+            }
+
+            string Dondur()
+            {
+                foreach (var item in tagListValue)
+                {
+                    Console.WriteLine("Tag: "+ " " +item.TagValue + " " +"Length" +" "+item.TagLentghValue + " " + "Value"+ " "+ item.DecimalTagValue);
+                }
+
+                return string.Empty;
+            }
+
+
+            break;
+
+        default:
+            break;
+    }
+    
+}
+
+
+
+
+
+
+
+
+
